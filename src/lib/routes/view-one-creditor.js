5839cb372fc84e7c9e71d7c1e653efaf9bcc067e
Jayne Doe <jayne.doe@dwp.gov.uk>
2020-02-14 14:05:52 +0000

    feat(link): add links and resulting pages

--- src/lib/routes/view-one-creditor.js
+++ src/lib/routes/view-one-creditor.js
@@ -1,12 +1,14 @@
 const renderer = require('../renderer').nunjucksRenderer();
 
-const renderResponse = (req, res, next) => {
+const renderWongaResponse = (req, res, next) => {
   res.contentType = 'text/html';
   res.header('content-type', 'text/html');
-  res.send(200, renderer.render('pages/view-one-creditor.njk', {...res.nunjucks}));
+  res.send(200, renderer.render('pages/view-wonga-creditor.njk', {...res.nunjucks}));
   next();
 };
 
 module.exports = (server) => {
-  server.get('/:creditorId', renderResponse);
+  server.get('/debt/wonga', renderWongaResponse);
+  server.get('/debt/barclaycard', renderWongaResponse);
+  server.get('/debt/msfinance', renderWongaResponse);
 };
